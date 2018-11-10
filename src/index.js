d015909e4450205a8f84c4aadef5399ef28c1bf8
Toni-Leigh Sharpe <toni.leigh@sage.com>
2018-11-10 13:17:53 +0000

    Moved provider into App file and removed FB default HTML

--- src/index.js
+++ src/index.js
@@ -4,19 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-import rootReducer from './reducers/reducers';
-
-const store = createStore(
-  rootReducer,
-  {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
 ReactDOM.render(
-  <Provider store={ store }><App /></Provider>, 
+  <App />,
   document.getElementById('root')
 );
 
