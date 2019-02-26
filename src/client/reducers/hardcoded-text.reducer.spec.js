9ac4412060f0b934dff0e05f729cb94b9eef0c23
Toni-Leigh Sharpe <toni.leigh@sage.com>
2019-02-26 19:13:09 +0000

    Covers hardcodeText reducer

---
+++ src/client/reducers/hardcoded-text.reducer.spec.js
@@ -0,0 +1,14 @@
+import { fromJS } from 'immutable';
+
+import hardcodedTextReducer from './hardcoded-text.reducer';
+import { HARDCODED_TEXT_RECEIVED } from '../actions/get-hardcoded-text.actions';
+
+test('hardcodedText reducer without state uses default', () => {
+  expect(hardcodedTextReducer(undefined, { type: 'STUBBED' })).toMatchSnapshot();
+});
+
+test('HARDCODED_TEXT_RECEIVED', () => {
+  const state = fromJS('');
+  const action = { type: HARDCODED_TEXT_RECEIVED, data: { hardcodedText: 'Test Hardcoded Text' } };
+  expect(hardcodedTextReducer(state, action)).toMatchSnapshot();
+});
